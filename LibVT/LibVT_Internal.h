e4c52acdf9185d3087a35090aa2ef1368b2e238a
Matthew Johnson-Roberson <mattjr@pc-200.acfr.usyd.edu.au>
2013-04-18 10:32:57 +1000

    updated osx libs for libmac

--- LibVT/LibVT_Internal.h
+++ LibVT/LibVT_Internal.h
@@ -74,6 +74,7 @@ You should have received a copy of the GNU Lesser General Public License along w
 	#else
 		#include <OpenGL/OpenGL.h>
 //		#include <Carbon/Carbon.h>
+        #include <ImageIO/ImageIO.h>
 	#endif
 	#include <dirent.h>
 	#if OPENCL_BUFFERREDUCTION
